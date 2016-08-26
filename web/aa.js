a39b1e3b63947896ce27a8c0829265dbd4021400
DaveToroyan <123456789davrob@gmail.com>
2016-08-26 22:14:19 +0400

    Add files via upload

---
+++ web/aa.js
@@ -0,0 +1,16 @@
+var check = function(){
+    var point = 0 ;
+if(document.getElementById("three").checked){point++};
+
+if(document.getElementById("four").checked){point++};
+
+if(document.getElementById("nine").checked){point++};
+
+if(document.getElementById("eleven").checked){point++};
+
+if(document.getElementById("sixteen").checked){point++};
+
+
+alert(point);
+}
+
